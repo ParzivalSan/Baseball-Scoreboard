2253843f090f58068e5dc20ca34546ef7fc5cf1f
Victor Cuevas <beisbol_1109@hotmail.com>
2026-03-27 09:23:30 +0100

    .bat

---
+++ Start.bat
@@ -0,0 +1,16 @@
+@echo off
+title Cargando Servidor Baseball Scoreboard...
+
+:: 1. Abrir el navegador en la URL de control
+:: Usamos 'start' para que se ejecute en paralelo al servidor
+echo Abriendo panel de control...
+start http://localhost:8000/control
+
+:: 2. Ejecutar uvicorn
+:: Si usas un entorno virtual, descomenta la siguiente linea:
+:: call .venv\Scripts\activate
+
+echo Iniciando servidor Uvicorn...
+uvicorn server:app --host 0.0.0.0 --port 8000
+
+pause
